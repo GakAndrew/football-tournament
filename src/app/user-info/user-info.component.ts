57e38cea8defc7bcd144f712643af6dcf42eda71
andrew_G <golovin.andre@mail.ru>
2024-06-11 13:30:54 +0300

    ref

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -5,21 +5,18 @@ import { MatCardModule } from '@angular/material/card';
 import { User } from '../../shared-lib/models/user.model';
 import { Router } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-user-info',
   standalone: true,
   imports: [CommonModule, MatCardModule],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserInfoComponent implements OnInit {
   users: User[] = [];
 
-  constructor(private clubService: ClubService, private routes: Router) { }
-
+  constructor(private clubService: ClubService, private routes: Router) {}
 
   ngOnInit(): void {
     this.users = this.clubService.users;
